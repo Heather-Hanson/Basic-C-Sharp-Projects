eb0e302e71451bab6e06184ef7d3e1fd1b8a0b06
Heather Hanson <74459872+Heather-Hanson@users.noreply.github.com>
2021-03-01 14:11:34 -0500

    Student Daily Report v1.0

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Academy of Learning Career College");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you in?");
+            string course = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            int page = Convert.ToInt32(pageNumber);
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\"");
+            string help = Console.ReadLine();
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string addFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            int hours = Convert.ToInt32(hoursStudied);
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
