64648857fd185d6e969fe2b31ed812b2f42c52db
Heather-Hanson <hjhnsn82@hotmail.com>
2021-03-10 18:15:43 -0400

    StringAssignment v1.0

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string con1 = "Hello, my name is Heather. ";
+            string con2 = "I live in Nova Scotia, Canada.";
+
+            Console.WriteLine(con1 + con2 + "  I have a kitten named, Phoebe.");
+
+            string province = "On";
+            Console.WriteLine("I lived in Toronto, " + province.ToUpper() + " for many years.");
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine();
+            sb.Append("The terror, which would not end for another twenty-eight years --");
+            sb.Append("if it ever did end - began, so far as I now or can tell,");
+            sb.Append("with a boat made from a sheet of newspaper floating down a gutter swollen with rain.");
+            sb.AppendLine();
+            sb.AppendLine();
+            sb.Append("\"It\" - Stephen King");
+
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Heather";
+            //string quote = "The man said, \\\"Hello\", Heather. \n Hello on a new line. \t Hello on a tab.";
+            //string fileName = "C:\\Users\\Heather"; 
+            //OR
+            //string fileName2 = @"C:\Users\Heather";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(name);
+
+            //string name = "Heather";
+            //name = "Jean";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Heather");
+
+            Console.WriteLine(sb);
+
+            Console.ReadLine();
+        }
+    }
+}
