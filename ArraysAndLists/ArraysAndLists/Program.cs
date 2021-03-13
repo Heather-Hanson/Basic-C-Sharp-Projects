b990e219cf0a33fa84615d44528896d2809ecb7e
Heather-Hanson <hjhnsn82@hotmail.com>
2021-03-13 11:44:15 -0400

    ArraysAssignment v1.0

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        //lists are called generics because you can create a list of any data type.  They use the "System.Collections.Generic" up above.
+
+        //Data type of a list, name of list, new instance of a list<data type>
+        //Lists are very adaptable and commonly used in programming. Many methods for lists.
+        //Arrays will be used when you have a fixed quantity that you know won't change grouped together because once you set the size it is difficult to change.
+        //Another time you will use arrays is if you are storing large quantities of something
+        //For most other cases, you will use a List.
+        List<int> intList = new List<int>();
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(10);
+
+        List<string> stringList = new List<string>();
+        stringList.Add("Hello");
+        stringList.Add("Heather");
+
+
+        Console.WriteLine(stringList[0]);
+        Console.ReadLine();
+
+
+        ////define data type of array int[], name array "numArray", instantiate array "new", "integer" [] = "array".  Length needs to be set for arrays in C# [5]
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        ////below is a shorter way of accomplishing above.
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 }; //the length is still set with this instance
+
+        ////below is the shortest way of creating an array 
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //// you can change the values of the array by referencing the index, but you can't change the size of an array once it is set.
+        //numArray2[5] = 650;
+
+        //Console.WriteLine(numArray2[5]);
+        //Console.ReadLine();
+        }
+    }
+
---
+++ ArraysAssignment/ArraysAssignment/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Program
+    {
+        static void Main()
+        {
+
+        //String array
+        string[] stringArray = { "Hello", "How are you?", "What's your name?", "How's the weather?" };
+
+        Console.WriteLine("Pick a number from 0 to 3");
+        int index = Convert.ToInt32(Console.ReadLine());
+
+        //Switch statement for each option and default when non-index number is entered
+        switch(index)
+        {
+            case 0:
+                Console.WriteLine(stringArray[index]);
+            break;
+
+            case 1:
+                Console.WriteLine(stringArray[index]);
+            break;
+
+            case 2:
+                Console.WriteLine(stringArray[index]);
+            break;
+
+            case 3:
+                Console.WriteLine(stringArray[index]);
+            break;
+
+            default:
+                Console.WriteLine("That number is not a valid choice");
+            break;
+        }
+
+        //Integer array
+        int[] intArray = { 5, 20, 36, 65};
+
+        Console.WriteLine("Pick a number from 0 to 3");
+        int index2 = Convert.ToInt32(Console.ReadLine());
+
+        //Switch statement for each option and a default when non-index number is entered
+        switch (index2)
+        {
+            case 0:
+                Console.WriteLine(intArray[index2]);
+                break;
+
+            case 1:
+                Console.WriteLine(intArray[index2]);
+                break;
+
+            case 2:
+                Console.WriteLine(intArray[index2]);
+                break;
+
+            case 3:
+                Console.WriteLine(intArray[index2]);
+                break;
+
+            default:
+                Console.WriteLine("That number is not a valid choice");
+                break;
+        }
+
+        //String list
+        List<string> stringList = new List<string>();
+        stringList.Add("Hello!");
+        stringList.Add("Have a nice day!");
+        stringList.Add("Hang in there!");
+        stringList.Add("You called an item from my list!");
+
+        //Ask user to pick a number and display string from that index
+        Console.WriteLine("Pick a number between 0 and 3");
+        int index3 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(stringList[index3]);    
+
+        Console.ReadLine();
+        }
+    }
+
