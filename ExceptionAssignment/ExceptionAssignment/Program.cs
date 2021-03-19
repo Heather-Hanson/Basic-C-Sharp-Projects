ffcea322c4a1bee3d589b12677842a36ff2e74f4
Heather-Hanson <hjhnsn82@hotmail.com>
2021-03-19 12:18:53 -0300

    ExceptionAssignment v1.0

---
+++ ExceptionAssignment/ExceptionAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        try
+        {
+            List<int> divisors = new List<int> { 2, 4, 6, 8 };
+            Console.WriteLine("Please enter a number to divide");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int divisor in divisors)
+            {
+                int quotient = divisor / dividend;
+                Console.WriteLine(divisor + " divided by " + dividend + " equals " + quotient);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Something went wrong: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("The program has emerged from the try/catch block");
+            Console.ReadLine();
+        }
+
+        }
+    }
+
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        try
+        {
+            Console.WriteLine("Pick a number.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick a second number.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+
+            Console.ReadLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number. ");
+            return; // the message won't run if you use return without a finally below
+        }
+        catch(DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally // can be helpful if you need to log something to a database. This will always run no matter what happens above.
+        {
+            Console.ReadLine();  
+        }
+        Console.ReadLine();
+        }
+    }
+
