4e0d8e7a7efee88531077cbc45d0aa4a0c03281f
Heather-Hanson <hjhnsn82@hotmail.com>
2021-03-20 17:36:41 -0300

    ClassesAndMethodsAssignment3 v1.0

---
+++ ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+    public class Math
+    { 
+        public void Add(int num1, int num2)
+        {
+            Console.WriteLine("{0} + {1} = {2}", num1, Convert.ToInt32(5), Convert.ToInt32(num1 + 5));
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ ClassesAndMethodsAssignment3/ClassesAndMethodsAssignment3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethodsAssignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.Add(10,7);
+            math.Add(num1: 13, num2: 9);
+
+            Console.ReadLine();
+        }
+    }
+}
