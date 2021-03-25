77ff17dec942d3d46cdfa415ebad52b0cf93b831
Heather-Hanson <hjhnsn82@hotmail.com>
2021-03-25 17:46:49 -0300

    ExceptionHandlingAssignment v1.0

---
+++ ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool validAnswer = false;
+                int age = 0;
+                while (!validAnswer)
+                {
+                    Console.WriteLine("Please enter the age you will be turning (or have turned) this year.");
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) throw new Exception();
+                    if (age < 0) throw new ExceptionHandling();
+                }
+                DateTime birthYear = DateTime.Now.AddYears(-age);
+                Console.WriteLine("The year you were born was: {0}", birthYear.Year);
+                Console.ReadLine();
+                return;
+            }
+            catch (ExceptionHandling)
+            {
+                Console.WriteLine("Please make sure you write a positive whole number (no decimals or fractions)");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong. Please report to System Administrator");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
