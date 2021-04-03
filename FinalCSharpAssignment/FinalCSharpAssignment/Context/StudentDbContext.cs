23926ead27e5df332acd2e94bf01b9701e344a6a
Heather-Hanson <hjhnsn82@hotmail.com>
2021-04-03 13:20:49 -0300

    FinalAssignment v1.0

---
+++ FinalCSharpAssignment/FinalCSharpAssignment/Context/StudentDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace FinalCSharpAssignment.Context
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(
+                new Student() { Id = 1, FirstName = "Heather", LastName = "Hanson", EmailAddress = "hjhnsn82@hotmail.com" });
+        }
+    }
+}
---
+++ FinalCSharpAssignment/FinalCSharpAssignment/Migrations/20210403161659_seeddata.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalCSharpAssignment.Migrations
+{
+    public partial class seeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "EmailAddress", "FirstName", "LastName" },
+                values: new object[] { 1, "hjhnsn82@hotmail.com", "Heather", "Hanson" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
